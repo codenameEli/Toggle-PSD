0ab59638dac6d54df1693ce3a72b2b76ffb3a5bf
Eli Dalbey <eli@launchdm.com>
2014-11-25 00:16:25 -0500

    Clean up mess. No more spaghetti code..

--- includes/javascript.js
+++ includes/javascript.js
@@ -1,150 +1,48 @@
 jQuery(document).ready(function($) {
 
-    // Find all of the list items in the admin tab
-    $togglePSDListItem = $('#wp-admin-bar-toggle-psd').find('.toggle-psd-item');
+    $.fn.turnOff = function() {
 
-    // Turn off the default behavior for a tag
-    $togglePSDListItem.on('click', 'a', function(e){
-        e.preventDefault();
-    });
-
-    // Add the buttons to the options
-    $togglePSDListItem.each(function(){
-        $(this).prepend('<button class="toggle-state-hidden">O</button>');
-    });
-
-    function comparePSDvsScreen(PSD) {
-        var theWindow = window.innerWidth;
-        var $PSD = $(PSD).width();
-        var PSDParent = $(PSD).parent();
-        console.log(theWindow);
-        console.log($PSD);
-
-        // Check if any of the PSD's are too wide for the screen
-        if ( $PSD < theWindow ) {
-            addWarningClass(PSDParent);
-        };
+        return $(this).removeClass('state-active').addClass('state-hidden');
     }
 
-    function addWarningClass(PSDParent) {
-        $(PSDParent).addClass('warning-too-big');
-        // alert("This overlay is wider than your screen currently. Make your screen bigger or adjust the size of the overlay.")
-    }
+    $.fn.positionOverlay = function() {
 
+        var overlay = $(this).find('img'),
+            overlayWidth = $(overlay).attr('width'),
+            position = '-' + ( overlayWidth / 2 ) + 'px';
+
+        return $(overlay).css( 'margin-left', position ).toggleState();
+    };
+
+    $.fn.toggleState = function() {
+
+        return $(this).toggleClass('state-active').toggleClass('state-hidden');
+    };
 
     // Find the data number of each admin bar
-    $.fn.matchUpDataNumbers = function() {
+    $.fn.displayOverlay = function() {
 
         var imageNumber = $(this).find('.toggle-psd-overlay').data('psd-number');
 
-        $('#listOfOverlays').find('.overlay-image').each(function(){
+        $('#listOfOverlays .overlay-image').each(function(){
 
             var overlayNumber = $(this).data('psd-number');
-            $overlayImage = $(this).find('img');
 
-            // This value is for the image positioning
-            $overlayWidth = $overlayImage.attr('width');
-            $positioningValue = '-' + $overlayWidth / 2 + 'px';
+            if ( overlayNumber !== imageNumber ) {
 
-            // Set the negative value of the image for positioning
-            $overlayImage.css( 'margin-left', $positioningValue );
-
-            // Check if it is already active
-            if ( $overlayImage.hasClass('overlay-active') ) {
-
-                $overlayImage.removeClass('overlay-active');
+                $(this).children().turnOff();
+                return;
             }
 
-            else {
-                // Ensure all of the overlay-active classes are removed
-                $overlayImage.removeClass('overlay-active');
+            $(this).positionOverlay();
 
-                if ( imageNumber === overlayNumber ) {
-                    $overlayImage.toggleClass('overlay-active');
-                }
-            }
         });
     };
 
-    // Remove state-active and add state-hidden
-    $.fn.turnOff = function() {
+    var $adminBar = $('#wp-admin-bar-toggle-psd');
 
-        $activeOverlayImage = $('#listOfOverlays').find('.overlay-active').removeClass('overlay-active');
-        clicks = 0;
-        return $(this).removeClass('state-active').addClass('state-hidden').turnOffAdminBar();
-    };
+    $adminBar.on( 'click', '.toggle-psd-item', function() {
 
-    // Remove state-active and add state-hidden
-    $.fn.turnOn = function() {
-
-        return $(this).addClass('state-active').removeClass('state-hidden').matchUpDataNumbers();
-    };
-
-    // Remove Admin Bar class
-    $.fn.turnOffAdminBar = function() {
-
-        $('#wp-admin-bar-toggle-psd').removeClass('top-level-overlay-active');
-    }
-
-    // Clicking the item
-    $togglePSDListItem.on('click', function(){
-
-        var PSD = $(this).find('.toggle-psd-overlay');
-        comparePSDvsScreen(PSD);
-
-        // Get state for the clicked item
-        $activeClass = $(this).hasClass('state-active');
-        $hiddenClass = $(this).hasClass('state-hidden');
-
-        // Turn every item off and ensure they have state-hidden
-        $togglePSDListItem.each(function(){
-
-            $(this).turnOff();
-        });
-
-        // Add active state to the clicked item
-        $(this).turnOn();
-
-        // Check if the item has state-active
-        // If so that means that this is their second click
-        // So toggle it off dammit
-        if ( $activeClass ) {
-
-            $(this).turnOff();
-        }
-    });
-
-    // Toggle the overlay via the top level admin bar button
-    var clicks = 0;
-
-    $('#wp-admin-bar-toggle-psd').on('click', function(){
-
-        // Check which list item is active
-        // If none are active, do not activate class
-        var $activeOverlay = $(this).find('.state-active').val();
-        if ( $activeOverlay !== 0 ) {
-
-          return false;
-        }
-
-        // If one is then toggle it up!
-        // $(this).toggleClass('top-level-overlay-active').find('li.state-active').toggleClass('state-active');
-        $(this).toggleClass('top-level-overlay-active');
-        // If you click on the admin button
-        // Find the active overlay
-        // Turn it off
-        if ( clicks <= 0 ) {
-
-            // Get the active-overlay once and set the state
-            // var clicks gets reset in turnOff function to reset state
-            $activeOverlayImage = $('#listOfOverlays').find('.overlay-active');
-        }
-
-        else {
-
-            $activeOverlayImage.toggleClass('overlay-active');
-        }
-
-        clicks++;
+        return $(this).toggleState().displayOverlay();
     });
 });
