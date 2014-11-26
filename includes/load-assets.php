86844ce27a1e414169f7bccdfc9ee06aaf185911
Eli Dalbey <eli@launchdm.com>
2014-11-26 01:25:26 -0500

    Add .min to file extension

--- includes/load-assets.php
+++ includes/load-assets.php
@@ -4,6 +4,6 @@ add_action( 'wp_enqueue_scripts', 'load_toggle_psd_assets' );
 
 function load_toggle_psd_assets() {
 
-    wp_enqueue_style( 'toggle-psd-css', plugins_url( 'assets/css/toggle-psd.css', dirname(__FILE__) ) );
-    wp_enqueue_script( 'toggle-psd-js', plugins_url( 'assets/js/toggle-psd.js', dirname(__FILE__) ), array( 'jquery' ) );
+    wp_enqueue_style( 'toggle-psd-css', plugins_url( 'assets/css/toggle-psd.min.css', dirname(__FILE__) ) );
+    wp_enqueue_script( 'toggle-psd-js', plugins_url( 'assets/js/toggle-psd.min.js', dirname(__FILE__) ), array( 'jquery' ) );
 }
