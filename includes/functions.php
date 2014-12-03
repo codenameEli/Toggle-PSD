19d733ac147cf5253e701f0957bc50f4e97cb730
Eli Dalbey <eli@launchdm.com>
2014-12-03 09:23:45 -0500

    Remove duplicate function call

--- includes/functions.php
+++ includes/functions.php
@@ -1,13 +1,1 @@
 <?php
-
-add_action( 'save_post', 'ce_update_transient' );
-
-function ce_update_transient( $post_id ) {
-
-    if ( 'toggle_psd' !== get_post_type( $post_id ) ) {
-
-        return;
-    }
-
-    delete_transient( 'toggle-psd-overlays' );
-}
